059df6f4ffab1f300db0a0c67fef40bbe1bd6d5d
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-04-19 14:03:25 -0500

    add correct details in reducer

--- src/components/LocationDetails/LocationDetails.jsx
+++ src/components/LocationDetails/LocationDetails.jsx
@@ -1,14 +1,24 @@
-import {useSelector} from 'react-redux'
+import {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 
 function LocationDetails() {
 
     const details = useSelector(store => store.details);
+    const dispatch = useDispatch();
+    const id = useParams().id;
 
+
+    useEffect(() => {
+        // Get locations of world based on world id -- maybe change to join code for more security
+        dispatch({ type: 'GET_DETAILS', payload: id });
+    }, [id]);
+
+    console.log(details);
   return (
     <div>
-        <h3>DETAILS</h3>
+        <h3>Name: {details.location_name}</h3>
     </div>
   )
 }
--- src/redux/sagas/details.saga.js
+++ src/redux/sagas/details.saga.js
@@ -4,7 +4,7 @@ import { put, takeEvery } from 'redux-saga/effects';
 function* getLocationDetails (action) {
     try {
         const details = yield axios.get(`/details/${action.payload}`);
-        yield put ({type: 'SET_DETAILS', payload: details.data});
+        yield put ({type: 'SET_DETAILS', payload: details.data[0]});
     } catch (err) {
         console.log(err);       
     }
