58ffe4cc4b3160cdd168f7fc64f0598cd8a65fe9
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-04-22 11:19:47 -0500

    add sweetalerts for world deletion

--- src/components/WorldItem/WorldItem.jsx
+++ src/components/WorldItem/WorldItem.jsx
@@ -1,7 +1,11 @@
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
 
+// Styles
 import './WorldItem.css'
+
+// Sweet Alert 2
+import Swal from 'sweetalert2'
 
 
 // Material UI
@@ -20,8 +24,35 @@ function WorldItem({ world }) {
     }
 
     const deleteWorld = () => {
+
         console.log('DELETING WORLD', world.world_name);
-        dispatch({ type: 'DELETE_WORLD', payload: world.id })
+
+        Swal.fire({
+            title: `Are you sure you want to delete ${world.world_name}?`,
+            text: "This cannot be undone.",
+            icon: 'warning',
+            background: '#C28340',
+            color: 'black',
+            showCancelButton: true,
+            confirmButtonColor: '#4A6F28',
+            cancelButtonColor: '#d43c2c',
+            confirmButtonText: 'Delete'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                dispatch({ type: 'DELETE_WORLD', payload: world.id })
+                Swal.fire({
+                    background: '#C28340',
+                    color: 'black',
+                    confirmButtonColor: '#4A6F28',
+                    title: 'Deleted!',
+                    text: `${world.world_name} has been deleted.`,
+                    icon: 'success'
+                })   
+            }
+        })
+
+
+
     }
 
     return (
@@ -36,17 +67,17 @@ function WorldItem({ world }) {
                         border: "1px solid black",
                     }}
                 >
-                    <span 
+                    <span
                         onClick={getLocations}
                         className='world-name'
                     >{world.world_name}</span>
                     <Button
-                    style={{
-                        justifyContent: 'center',
-                        alignItems: 'flex-end'
-                    
-                    }}
-                    align=''
+                        style={{
+                            justifyContent: 'center',
+                            alignItems: 'flex-end'
+
+                        }}
+                        align=''
                         onClick={deleteWorld}
                     ><DeleteIcon />
                     </Button>
