804c5c0449e6c9a486b55a142f8a8a5d4e6bf35a
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-05-02 14:47:01 -0500

    remove about redirect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,9 @@ function Header({ pageTitle }) {
 
   const history = useHistory();
 
-  const goToAbout = () => {
-    history.push('/about')
-  }
-
   return (
     <div className='header'>
-      <h2 onClick={goToAbout}>Villager Cartography</h2>
+      <h2>Villager Cartography</h2>
       <h1 className='title'>{pageTitle}</h1>
 
     </div>
